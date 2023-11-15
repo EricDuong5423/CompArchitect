8c29b47c46117474a571a831d1e1fda94a077939
EricDuong5423 <91255998+EricDuong5423@users.noreply.github.com>
2023-11-15 22:26:26 +0700

    Done merge but ...
    can't divide array into 1/2 like GOJO

--- MergeSort.asm
+++ MergeSort.asm
@@ -4,24 +4,89 @@
 	originalArraymsg: .asciiz "Mang ban dau: "
 	space: .asciiz " "
 	endl: .asciiz "\n"
-	
+	tempArray: .word 0,0,0,0,0,0
 .text
 j main
+	#In thong bao mang ban dau
+	li $v0, 4
+	la $a0, originalArraymsg
+	syscall
+	#In mang
+	jal print_array
+merge:
+	move $t0, $a1
+	move $t1, $a3
+	la $s1, tempArray
+	
+	#while(start < mid && mid < end) = while($t0 < $a3 && $t1 < $a2)
+	while:
+		bge $t0, $a3, while1 #$t0 < $a3 ? 
+		bge $t1, $a2, while2 #$t1 < $a2 ?
+		lw $t2, 0($t0) #$t2 = array[left] (left < mid)
+		lw $t3, 0($t1) #$t3 = array[right] (right < end)
+		ble $t2, $t3, left #$t2 >= $t3 ? left : right
+		j right
+		left:
+			sw $t2, 0($s1) #$t4[cur_index] = $t2
+			addi $t0, $t0, 4 #left++
+			j end_cond
+		right: 
+			sw $t3, 0($s1) #$t4[cur_index] = $t3
+			addi $t1, $t1, 4 #right++
+		end_cond:
+			addi $s1, $s1, 4
+			j while
+	#TH: right con phan tu
+	while1:
+		bge $t1, $a2, end_merge
+		
+		lw $t3, 0($t1)
+		sw $t3, 0($s1)
+		
+		addi $t1, $t1, 4
+		addi $s1, $s1, 4
+		
+		j while1
+	#TH: left con phan tu
+	while2:
+		bge $t0, $a3, end_merge
+		
+		lw $t2, 0($t0)
+		sw $t2, 0($s1) 
+		
+		addi $t0, $t0, 4
+		addi $s1, $s1, 4
+		
+		j while2
+	end_merge:
+		la $s0, tempArray
+		move $t0, $a1
+		sub $s2, $s1, $s0
+	save_originArray:
+		beq $s0, $s1, end_save_originArray
+		lw $t1, 0($s0)
+		sw $t1, 0($t0)
+		addi $t0, $t0, 4
+		addi $s0, $s0, 4
+		j save_originArray
+	end_save_originArray:
+	jr $ra
 merge_sort:
 	addi $sp, $sp, -16
-	sw $ra, 12($sp)
-	sw $a1, 8($sp)
-	sw $a2, 4($sp)
+	sw $ra, 12($sp)#Dia chi quay ve $ra
+	sw $a1, 8($sp)#$a1 la dau mang
+	sw $a2, 4($sp)#$a2 la cuoi mang
+	
 	#$t0 = array.size()
 	sub $t0, $a2, $a1
-	li $t1, 4
-	div $t0, $t0, $t1
+	srl $t0, $t0, 3
+	sll $t0, $t0, 2
 	
 	#if(size < 1) thi ko chia doi mang nua
 	ble $t0, 1, end_merge_sort
 	
 	#$a1 = mid_array
-	add $a2, $a1, $t0
+	add $a2, $t0, $a1
 	sw $a2, 0($sp)
 	
 	#Ð? quy ph?n bên trái
@@ -36,11 +101,21 @@ merge_sort:
 	lw $a1, 8($sp)
 	lw $a2, 4($sp)
 	lw $a3, 0($sp)
-	j print_array
+	jal merge
+	
+	#In mang
+	lw $a1, 8($sp)
+	lw $a2, 4($sp)
+	jal print_array
+	
+	#Ket thuc merge_sort
 	end_merge_sort:
+	lw $ra, 12($sp)
+	addi $sp, $sp, 16
+	jr $ra
+	
 print_array:
 	move $s0, $a1
-	
 	loop_print:
 		beq $s0, $a2, end_print #Dieu kien dung vong lap 
 		#In phan tu a[i]
@@ -58,19 +133,10 @@ print_array:
 	li $v0, 4
 	la $a0, endl
 	syscall
-	
 	jr $ra
 main:
 	la $a1, inputArray #$a1 = &array.begin()
 	lw $a2, inputSize #So luong phan tu
-	sll $a2, $a2, 2
-	add $a2, $a1, $a2
-	
-	#In thong bao mang ban dau
-	li $v0, 4
-	la $a0, originalArraymsg
-	syscall
-	#In mang
-	jal print_array
+	addi $a2, $a1, 24
 	#Merge_sort
-	
+	jal merge_sort
